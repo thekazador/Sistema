6c30e403a46eda6cceb4a8b1e80b77c261edac85
TheKazador <thekazador@gmail.com>
2014-12-14 23:14:31 -0300

    0.05

--- Sistema/src/DB/Methods.java
+++ Sistema/src/DB/Methods.java
@@ -8,30 +8,29 @@ import java.util.Properties;
 
 public class Methods {
 
-	public static void Connect() {
+	public static void DBStart() {
 		
+		// seteo de la carpeta de la db
 		Properties p = System.getProperties();
 		p.setProperty("derby.system.home", ".");
 		
+		// configuracion de la db
 		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
 	    String dbName = "/MyDB";
 	    String dbParam = "create=true";
 	    String connectionURL = "jdbc:derby:" + dbName + ";" + dbParam;
+	    
+	    // conexion a la db
 	    Connection conn = null;
-	 
-	    try {
-	      Class.forName(driver);
-	    }
-	    catch(java.lang.ClassNotFoundException e) {
-	      e.printStackTrace();
-	    }
-
+	    try { Class.forName(driver); }
+	    catch(java.lang.ClassNotFoundException e) { e.printStackTrace(); }
 	    try {
 	        conn = DriverManager.getConnection(connectionURL);
 	   
 	        Statement st = conn.createStatement();
 	        System.out.println("Conectado a la DB: '" + dbName + "'");
             
+	        // busco la tabla
 	        ResultSet results = st.executeQuery("select * from SYS.SYSTABLES");
         	boolean tableok = false;
             while(results.next()) {
@@ -41,11 +40,13 @@ public class Methods {
             	}
             }
 	        
+            // creo la tabla si no la encuentro
 	        if (tableok == false) {
 	        	st.executeUpdate("create table users(id_user INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), name_user varchar(20), pass_user varchar(20))");
 	        	st.executeUpdate("insert into users(name_user, pass_user) values ('admin', 'admin123')");
 	        }
 	        
+	        // temporal - listado de usuarios
 	        results = st.executeQuery("select * from users");
             while(results.next()) {
                 int id = results.getInt(1);
@@ -59,14 +60,6 @@ public class Methods {
 	    catch (Throwable e)  {
 	    	System.out.println("Error al crear la base de datos '" + dbName + "'");
 	    	e.printStackTrace();
-	    }
-	    finally {
-	    	try {
-	    		// conn.close();
-	    		// test 2
-	    		// test 3
-	    	}
-	        catch (Throwable t){}
 	    }
 	}		
 }
